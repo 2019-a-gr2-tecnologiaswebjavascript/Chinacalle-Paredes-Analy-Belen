fde38fdd55b3d08fe0568c582113841f3d94c5f8
Analy Chinacalle <analy_chinacalle@hotmail.com>
2019-05-29 18:11:14 -0500

    chat

--- 02-ionic/wA/src/app/mensaje/mensaje.page.ts
+++ 02-ionic/wA/src/app/mensaje/mensaje.page.ts
@@ -9,10 +9,47 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class MensajePage implements OnInit {
 
   constructor(
-    private readonly router Router,
+    private readonly _activatedRoute: ActivatedRoute
   ) { }
 
+  nombre: any;
+  mensaje: string;
+  respuesta: string;
+  respuestas = [
+    {
+      mensaje: 'soy una respuesta'
+    },
+    {
+      mensaje: 'estoy escribiendo'
+    },
+    {
+      mensaje: 'ojalá pasemos'
+    },
+  ];
+
+  mensajeando(texto){
+
+  }
+
+
   ngOnInit() {
+    const parametros$ = this._activatedRoute.params;
+
+    parametros$
+    .subscribe(
+      (parametros)=>{  //OK TRY
+        this.nombre = parametros['nombre'];
+        console.log('Parametros', this.nombre);
+      },
+      (error)=>{    //CATCH
+        console.log('Error: ', error);
+      },
+      ()=>{ //Completado --> finally
+        console.log('completo');
+      }
+    )
+
+
   }
 
 }
